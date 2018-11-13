f5d93c9997dfdd8cd93b649e59fcceae3db8cfbd
Jahzeel Ramos <jp.ramos@irri.org>
2018-11-13 22:06:17 +0800

    Remove executable path

--- extractorui_datasets_test.py
+++ extractorui_datasets_test.py
@@ -75,7 +75,7 @@ class ExtractorUITest(unittest.TestCase):
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
 
-        self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=options)    
+        self.driver = webdriver.Chrome(chrome_options=options)    
 
     # log in
     def test_log_in(self):
